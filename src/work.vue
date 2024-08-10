69b605a194ae56ee369195ce03a7f35ad6802f05
Harsh-Kumar-Sharma <gaurharsh1737@gmail.com>
2024-08-11 00:56:57 +0530

    make footer changes

--- src/work.vue
+++ src/work.vue
@@ -65,13 +65,19 @@
     </div>
   </div>
   <footer class="bg text-white py-4">
-  <div class="container mx-auto flex justify-between items-center">
-    <p class="text-sm">© 2024 Harsh Kumar Sharma. All rights reserved.</p>
-    <ul class="flex space-x-4">
-      <li> <a href="tel:+91 8979571118" class="text-white hover:text-gray-500">Mobile: +91 8979571118</a></li>
-      <li><span class="text-white">|</span></li>
-      <li><a href="mailto:info@example.com" class="text-white hover:text-gray-500">Email: gaurharsh1737@gmail.com</a></li>
-    </ul>
+  <div class="container mx-auto px-4">
+    <div class="flex flex-col md:flex-row md:justify-between items-center">
+      <p class="text-sm text-center md:text-left mb-2 md:mb-0">© 2024 Harsh Kumar Sharma. All rights reserved.</p>
+      <ul class="flex flex-col md:flex-row md:space-x-4 text-center md:text-left">
+        <li class="mb-2 md:mb-0">
+          <a href="tel:+91 8979571118" class="text-white hover:text-gray-400">Mobile: +91 8979571118</a>
+        </li>
+        <li class="md:hidden"><span class="text-white">|</span></li>
+        <li>
+          <a href="mailto:gaurharsh1737@gmail.com" class="text-white hover:text-gray-400">Email: gaurharsh1737@gmail.com</a>
+        </li>
+      </ul>
+    </div>
   </div>
 </footer>
 </template>
